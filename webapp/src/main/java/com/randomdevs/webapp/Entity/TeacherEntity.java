e2bae361f7dd1f166b362172f251389dac382068
Jaymark Carba <jaymark.carba@cit.edu>
2022-11-22 23:46:30 +0800

    Add relationship to resources

--- webapp/src/main/java/com/randomdevs/webapp/Entity/TeacherEntity.java
+++ webapp/src/main/java/com/randomdevs/webapp/Entity/TeacherEntity.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "tbl_teacher")
 public class TeacherEntity {
@@ -25,11 +27,15 @@ public class TeacherEntity {
 	@OneToMany(cascade = CascadeType.MERGE)
 	private Set<CourseEntity> course;
 	
+	@JsonIgnore
+	@OneToMany(mappedBy="teacherRes", cascade = CascadeType.ALL)
+	Set<ResourcesEntity> resources;
+	
 	//constructor
 	public TeacherEntity() {}
 
 	public TeacherEntity(int id, String firstname, String lastname, String middlename, String contact,
-			Set<CourseEntity> course) {
+			Set<CourseEntity> course, Set<ResourcesEntity> resources) {
 		super();
 		this.id = id;
 		this.firstname = firstname;
@@ -37,6 +43,7 @@ public class TeacherEntity {
 		this.middlename = middlename;
 		this.contact = contact;
 		this.course = course;
+		this.resources = resources;
 	}
 
 	public int getId() {
@@ -70,4 +77,13 @@ public class TeacherEntity {
 	public void setCourse(Set<CourseEntity> course) {
 		this.course = course;
 	}
+
+	public Set<ResourcesEntity> getResources() {
+		return resources;
+	}
+
+	public void setResources(Set<ResourcesEntity> resources) {
+		this.resources = resources;
+	}
+
 }
